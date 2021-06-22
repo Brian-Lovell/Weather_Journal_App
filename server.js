14a50d756c80ef8dae4732836ddf9ac3b0ada9ed
Brian Lovell <brian.lovell@outlook.com>
2021-06-22 10:38:00 -0400

    Add express

--- server.js
+++ server.js
@@ -12,3 +12,14 @@ const server = http.createServer((req, res) => {
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send('GET request to homepage')
+});
+
+app.post('/', function (req, res) {
+    res.send('Post request to homepage')
+});
